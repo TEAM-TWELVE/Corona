22b5711a52edc4e968ce45e484f0454c5aa6dad8
maleneH <themalenehansen@gmail.com>
2020-12-09 14:13:52 +0100

    syntax corrections

---
+++ scraping_script.py
@@ -0,0 +1,30 @@
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = "https://who.maps.arcgis.com/apps/opsdashboard/index.html#/ead3c6475654481ca51c248d52ab9c61"
+
+#fetch html with selenium
+driver = webdriver.Firefox()
+driver.get(url)
+html = driver.execute_script("return document.documentElement.outerHTML")
+
+#Use BeautifulSoup for working with html
+soup = BeautifulSoup(html, "html.parser")
+
+
+parties_list = soup.find("div", id="ember44").find_next("div").nav.span.div.find_next("div")
+
+country = parties_list.find("strong").get_text()
+
+colorUrl = parties_list.find("img").get('src')  
+colorNumber = colorUrl[-5] # colors: 1:green, 2: yellow, 3:red
+
+
+cases = parties_list.strong.find_next("strong").get_text()
+
+#print(parties_list.prettify())
+
+print(country)
+print(colorNumber)
