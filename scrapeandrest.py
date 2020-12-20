ad77a5ec5af010fcefc62cd00e15af3fbf4c6139
Malene <malene1105@gmail.com>
2020-12-20 09:59:28 +0100

    json output updatet toarray format

--- scrapeandrest.py
+++ scrapeandrest.py
@@ -9,15 +9,15 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 
 json_file = "data.json"
-x=datetime.today()
+x = datetime.today()
 y = x.replace(day=x.day, hour=1, minute=0, second=0, microsecond=0) + timedelta(days=1)
-delta_t=y-x
+delta_t = y - x
 
-secs=delta_t.total_seconds()
+secs = delta_t.total_seconds()
 
 
 def get_covid_color(imgURL):
-    number = imgURL[-5] # colors: 1:green, 2: yellow, 3:red
+    number = imgURL[-5]  # colors: 1:green, 2: yellow, 3:red
     if number == "1":
         return "green"
     elif number == "2":
@@ -35,48 +35,51 @@ def clean_country_name(country):
 
 
 def save_to_json(covid_dict):
-    #prepare json file ready for updates
-    currenct_json = open(json_file)
-    json_object = json.load(currenct_json)
-    currenct_json.close()
-    #check for color updates
+    # prepare json file ready for updates
+    current_json = open(json_file)
+    json_object = json.load(current_json)
+    current_json.close()
+    # check for color updates
     for dict_country, dict_color in covid_dict.items():
         json_color = json_object.get(dict_country)
         if json_color != dict_color:
             json_object[dict_country] = dict_color
-            #log change
+            # log change
             print(dict_country, " .. has changed from ", json_color, " to ", dict_color)
-            #close and save json file        
-            currenct_json = open (json_file, "w")
-            json.dump(json_object, currenct_json)
-    currenct_json.close()
-            
+            # close and save json file
+            current_json = open(json_file, "w")
+            json.dump(json_object, current_json)
+    current_json.close()
 
-
-        
 
 def web_scrape():
     url = "https://who.maps.arcgis.com/apps/opsdashboard/index.html#/ead3c6475654481ca51c248d52ab9c61"
-    #make firefox headless
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.set_headless()
-    #fetch html using selenium
-    driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+    # make chrome headless
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.set_headless()
+    # fetch html using selenium
+    driver = webdriver.Firefox(firefox_options=firefox_options)
     driver.get(url)
-    time.sleep(20)
+    time.sleep(10)
     html = driver.execute_script("return document.documentElement.outerHTML")
-    #Use BeautifulSoup for working with html
+    # Use BeautifulSoup for working with html
     soup = BeautifulSoup(html, "html.parser")
     covid_soup = soup.find("div", id="ember44").div.nav.find_all("span", class_="flex-horizontal")
-    covid_dict = {}
+    # covid_dict = {}
+    country, color = [], []
     for i in covid_soup:
-        country = i.find("strong").get_text(strip=True)
-        country = clean_country_name(country)
-        imgURL = i.p.find_next("p").find_next("p").find("img").get('src') 
-        color = get_covid_color(imgURL)
-        covid_dict[country] = color
-    save_to_json(covid_dict)
-    
+        _country = i.find("strong").get_text(strip=True)
+        _country = clean_country_name(_country)
+        country.append(_country)
+        imgURL = i.p.find_next("p").find_next("p").find("img").get('src')
+        _color = get_covid_color(imgURL)
+        color.append(_color)
+    # save as json array
+    countries = [{"country": _country, "color": _color} for _country, _color in zip(country, color)]
+    with open(json_file, 'w') as f:
+        json.dump(countries, f)
+    # Printing in JSON format
+    # print(json.dumps(countries))
 
 
 
@@ -89,21 +92,21 @@ time.sleep(20)
 app = Flask(__name__)
 api = Api(app)
 
-#data for end point  
-f = open(json_file,)
-thisdict =  json.load(f) 
-f.close() 
+# data for end point
+f = open(json_file, )
+thisdict = json.load(f)
+f.close()
 
-#Rest endpoint
+
+# Rest endpoint
 class coronaStatus(Resource):
     def get(self):
         st = request.args.get('land')
         return {"status": thisdict.get(st)}
 
-api.add_resource(coronaStatus, "/coronastatus") 
 
+api.add_resource(coronaStatus, "/coronastatus")
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    # app.run(debug=True)
     app.run()
-
